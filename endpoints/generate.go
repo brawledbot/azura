c08073c719c91bcab826ce1d69b7db577914d4b4
Jan-Niklas (on MacOS) <120685902+estryaog@users.noreply.github.com>
2024-07-04 23:48:33 +0200

    Implemented endpoint: generate

---
+++ endpoints/generate.go
@@ -0,0 +1,53 @@
+package endpoints
+
+import (
+	"fmt"
+
+	"github.com/brawledbot/azura"
+	"github.com/brawledbot/azura/parser"
+	"github.com/brawledbot/azura/types"
+)
+
+// GenerateEndpoint represents the endpoint for image generation API operations.
+type GenerateEndpoint struct {
+	client *azura.Client // client is the HTTP client used to make requests.
+}
+
+// NewGenerateEndpoint creates a new GenerateEndpoint instance.
+func NewGenerateEndpoint(client *azura.Client) *GenerateEndpoint {
+	return &GenerateEndpoint{client: client}
+}
+
+// Generate generates an image based on the player tag and render type.
+func (g *GenerateEndpoint) Generate(playerTag string, renderType string) (*types.GeneratedImage, error) {
+	endpoint := fmt.Sprintf("/v1/generate/%s?renderType=%s", playerTag, renderType)
+	response, err := g.client.DoRequest("GET", endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var generatedImage types.GeneratedImage
+	result, err := parser.ParseDataToType(response.Data, &generatedImage)
+	if err != nil {
+		return nil, err
+	}
+
+	return result.(*types.GeneratedImage), nil
+}
+
+// GenerateWithConfigURL generates an image with custom configuration specified by a URL.
+func (g *GenerateEndpoint) GenerateWithConfigURL(playerTag string, renderType string, configURL string) (*types.GeneratedImage, error) {
+	endpoint := fmt.Sprintf("/v1/generate/%s?renderType=%s&custom=%s", playerTag, renderType, configURL)
+	response, err := g.client.DoRequest("GET", endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var generatedImage types.GeneratedImage
+	result, err := parser.ParseDataToType(response.Data, &generatedImage)
+	if err != nil {
+		return nil, err
+	}
+
+	return result.(*types.GeneratedImage), nil
+}
