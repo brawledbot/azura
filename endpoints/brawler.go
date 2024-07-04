38cb9bd759e1fbb2038b7f88c71f16bd3c96a852
Jan-Niklas (on MacOS) <120685902+estryaog@users.noreply.github.com>
2024-07-04 23:48:55 +0200

    Implemented endpoint: brawler

---
+++ endpoints/brawler.go
@@ -0,0 +1,70 @@
+package endpoints
+
+import (
+	"fmt"
+
+	"github.com/brawledbot/azura"
+	"github.com/brawledbot/azura/parser"
+	"github.com/brawledbot/azura/types"
+)
+
+// BrawlerEndpoint represents the endpoint for brawler-related API operations.
+type BrawlerEndpoint struct {
+	client *azura.Client // client is the HTTP client used to make requests.
+}
+
+// NewBrawlerEndpoint creates a new BrawlerEndpoint instance.
+func NewBrawlerEndpoint(client *azura.Client) *BrawlerEndpoint {
+	return &BrawlerEndpoint{client: client}
+}
+
+// GetBrawlerByID retrieves a brawler by their ID.
+func (b *BrawlerEndpoint) GetBrawlerByID(id int) (*types.Brawler, error) {
+	endpoint := fmt.Sprintf("/v1/brawler/id/%d", id)
+	response, err := b.client.DoRequest("GET", endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var brawler types.Brawler
+	result, err := parser.ParseDataToType(response.Data, &brawler)
+	if err != nil {
+		return nil, err
+	}
+
+	return result.(*types.Brawler), nil
+}
+
+// GetBrawlerByName retrieves a brawler by their name.
+func (b *BrawlerEndpoint) GetBrawlerByName(name string) (*types.Brawler, error) {
+	endpoint := fmt.Sprintf("/v1/brawler/name/%s", name)
+	response, err := b.client.DoRequest("GET", endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var brawler types.Brawler
+	result, err := parser.ParseDataToType(response.Data, &brawler)
+	if err != nil {
+		return nil, err
+	}
+
+	return result.(*types.Brawler), nil
+}
+
+// GetAllBrawlers retrieves all available brawlers.
+func (b *BrawlerEndpoint) GetAllBrawlers() ([]types.Brawler, error) {
+	endpoint := "/v1/brawlers"
+	response, err := b.client.DoRequest("GET", endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var brawlers []types.Brawler
+	result, err := parser.ParseDataToType(response.Data, &brawlers)
+	if err != nil {
+		return nil, err
+	}
+
+	return result.([]types.Brawler), nil
+}
