8b20ac67c7d8b8dc97214e96b9ae5736c49b4ac0
Jan-Niklas (on MacOS) <120685902+estryaog@users.noreply.github.com>
2024-07-04 23:48:45 +0200

    Implemented endpoint: suspension

---
+++ endpoints/suspension.go
@@ -0,0 +1,62 @@
+package endpoints
+
+import (
+	"fmt"
+
+	"github.com/brawledbot/azura"
+	"github.com/brawledbot/azura/parser"
+	"github.com/brawledbot/azura/types"
+)
+
+// SuspensionEndpoint represents the endpoint for suspension-related API operations.
+type SuspensionEndpoint struct {
+	client *azura.Client // client is the HTTP client used to make requests.
+}
+
+// NewSuspensionEndpoint creates a new SuspensionEndpoint instance.
+func NewSuspensionEndpoint(client *azura.Client) *SuspensionEndpoint {
+	return &SuspensionEndpoint{client: client}
+}
+
+// GetSuspension retrieves suspension details for a user by their ID.
+func (ss *SuspensionEndpoint) GetSuspension(id string) (*types.Suspension, error) {
+	endpoint := fmt.Sprintf("/v1/suspension/id/%s", id)
+	response, err := ss.client.DoRequest("GET", endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var suspension types.Suspension
+	result, err := parser.ParseDataToType(response.Data, &suspension)
+	if err != nil {
+		return nil, err
+	}
+
+	return result.(*types.Suspension), nil
+}
+
+// SuspendUser suspends a user by their ID with a specified reason.
+// Returns the suspension details or an error if the suspension fails.
+func (ss *SuspensionEndpoint) SuspendUser(id, reason string) (*types.Suspension, error) {
+	endpoint := fmt.Sprintf("/v1/suspend/%s?reason=%s", id, reason)
+	response, err := ss.client.DoRequest("POST", endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var suspension types.Suspension
+	result, err := parser.ParseDataToType(response.Data, &suspension)
+	if err != nil {
+		return nil, err
+	}
+
+	return result.(*types.Suspension), nil
+}
+
+// UnsuspendUser unsuspends a user by their ID.
+// Returns an error if the unsuspension fails.
+func (ss *SuspensionEndpoint) UnsuspendUser(id string) error {
+	endpoint := fmt.Sprintf("/v1/unsuspend/%s", id)
+	_, err := ss.client.DoRequest("POST", endpoint, nil)
+	return err
+}
